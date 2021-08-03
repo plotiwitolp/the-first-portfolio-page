7003072a89bafe583ac541682924032035635127
plotiwitolp <plotiwitolp@gmail.com>
2021-08-03 19:52:24 +0300

    added the-first-portfolio-page/ in style link

--- header.js
+++ header.js
@@ -6,7 +6,7 @@ document.write(
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pet-projects by Ivan Plotnikov</title>
-    <link rel="stylesheet" href="${location.origin}/style.css">
+    <link rel="stylesheet" href="the-first-portfolio-page/${location.origin}/style.css">
 </head>
 <body>
 <div class="content-wrapper">
