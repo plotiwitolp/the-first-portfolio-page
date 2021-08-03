86a0e6ebbc052c3c3936c0bb02e0744db3bd55aa
plotiwitolp <plotiwitolp@gmail.com>
2021-08-03 19:39:39 +0300

    changed from php tamplate to js (crutches)

---
+++ footer.js
@@ -0,0 +1,17 @@
+document.body.innerHTML+=
+        `</div>
+    </div>
+    </section>
+    <footer>
+    <div class="wrapper">
+        <p>
+            <a href="https://www.instagram.com/vanyok90/" target="_blank" class="inst-image"><b>my</b>Instagram</a>
+        </p>
+        © 2021 all rights reserved
+    </div>
+    </footer>
+    </div>
+    <script src="${location.origin}/script.js"></script>
+    </body>
+    </html>
+    `
---
+++ header.js
@@ -0,0 +1,28 @@
+document.write(
+    `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet-projects by Ivan Plotnikov</title>
+    <link rel="stylesheet" href="${location.origin}/style.css">
+</head>
+<body>
+<div class="content-wrapper">
+    <section>
+        <div class="left-block">
+            <div class="wrapper">
+                <nav id="main-nav">
+                    <ul>
+                        <li><a href="${location.origin}/index.html"><b> Home</b></a></li>
+                        <li><a href="${location.origin}/02-tic-tac-toe-alternative/index.html">Tic-Tac-Toe Alternative</a></li>
+                    <li><a href="${location.origin}/01-simple-calculator/index.html">Simple calculator</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+<div class="right-block">
+    <div class="wrapper">`)
+
+
